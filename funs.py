00ad1018c568ff37fc2e4a314b98d89f88f9eee1
radoslawik <radoslawpudelko96@gmail.com>
2019-11-18 16:49:16 +0100

    Added error handling to testing connection

--- funs.py
+++ funs.py
@@ -7,26 +7,31 @@ Created on Fri Nov 15 10:55:58 2019
 
 import pymysql
 
-def add(x,y):
-    return x+y
+def testConnection(address, username, password, database): 
+    try:
+        #open database connection 
+        conn = pymysql.connect(address, username, password, database)
+        # prepare a cursor object using cursor() method
+        cursor = conn.cursor()
+        # execute SQL query using execute() method.
+        cursor.execute("SELECT VERSION()")
+        # Fetch a single row using fetchone() method.
+        data = cursor.fetchone()
+        print ("Database version: %s " % data)
+        # disconnect from server
+        conn.close()
+    except pymysql.InternalError as e:
+        print('ERROR: {!r}, errno is {}'.format(e, e.args[0]))
+        return e.args[0]
+    except pymysql.OperationalError as e:
+        print('ERROR: {!r}, errno is {}'.format(e, e.args[0]))
+        return e.args[0]
+    
+    return 0
     
 def main():
-    #open database connection
-    db = pymysql.connect("localhost","root","","company" )
-    print("Connected")
-      
-    # prepare a cursor object using cursor() method
-    cursor = db.cursor()
-
-    # execute SQL query using execute() method.
-    cursor.execute("SELECT VERSION()")
-
-    # Fetch a single row using fetchone() method.
-    data = cursor.fetchone()
-    print ("Database version : %s " % data)
-
-    # disconnect from server
-    db.close()
+    if(testConnection("localhost","root","","devops") == 0):
+        print("Connection tested with success") 
     
 if __name__ == "__main__":
     main()
--- test/test_funs.py
+++ test/test_funs.py
@@ -5,11 +5,20 @@ Created on Fri Nov 15 10:57:14 2019
 @author: Radek
 """
 import sys
-
 sys.path.insert(1, 'D:/Dokumenty/Studia/Master2/DevOps/PytestProject')
 import funs
 
-def test_add():
-    assert funs.add(1,2) == 3
+address = "localhost"
+user = "root"
+password = ""
+database = "devops"
+
+def test_testConnection():  
+    assert funs.testConnection(address, user, password, "incorrect_db_name") == 1049
+    assert funs.testConnection(address, user, "incorrect_password", database) == 1045
+    assert funs.testConnection(address, "wrong_user", password, database) == 1045
+    assert funs.testConnection("made_up_address", user, password, database) == 2003
+    assert funs.testConnection(address, user, password, database) == 0
+    
 
 
