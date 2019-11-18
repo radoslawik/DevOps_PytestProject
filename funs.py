1bd1bffa01b41b3b0516165236b327b760846264
radoslawik <radoslawpudelko96@gmail.com>
2019-11-18 19:45:14 +0100

    Added reading data from table and commented code

--- funs.py
+++ funs.py
@@ -2,49 +2,31 @@
 """
 Created on Fri Nov 15 10:55:58 2019
 
-@author: Radek
+@author: Radoslaw Pudelko
 """
 
+# library providing connection to mySQL database
 import pymysql
 
-def testConnection(address, username, password, database): 
-    try:
-        #open database connection 
-        conn = pymysql.connect(address, username, password, database)
-        # prepare a cursor object using cursor() method
-        cursor = conn.cursor()
-        # execute SQL query using execute() method.
-        cursor.execute("SELECT VERSION()")
-        # Fetch a single row using fetchone() method.
-        data = cursor.fetchone()
-        print ("Database version: %s " % data)
-        # disconnect from server
-        conn.close()
-    except pymysql.InternalError as e:
-        print('ERROR: {!r}, errno is {}'.format(e, e.args[0]))
-        return False
-    except pymysql.OperationalError as e:
-        print('ERROR: {!r}, errno is {}'.format(e, e.args[0]))
-        return False
-    except:
-        return False
-        
-    return True
+'''
+Checks connection with database and prints it's version
+'''
+def testConnection(address, username, password, database):
     
-def createTable(address, username, password, database, table):
+    command = "SELECT VERSION()"
     try:
-        # Open database connection
+        # open database connection 
         conn = pymysql.connect(address, username, password, database)
-        # prepare a cursor object using cursor() method
+        # prepare a cursor object
         cursor = conn.cursor()
-        # Drop table if it already exist using execute() method.
-        check_command = "DROP TABLE IF EXISTS " + table;
-        cursor.execute(check_command)
-        # Create table as per requirement
-        command = "CREATE TABLE " + table + "(NAME  VARCHAR(30) NOT NULL, NUMBER INT)"
+        # execute SQL query
         cursor.execute(command)
+        # fetch a single row data
+        data = cursor.fetchone()
+        print("Database version: %s" % data)
         # disconnect from server
         conn.close()
+    # error handling
     except pymysql.InternalError as e:
         print('ERROR: {!r}, errno is {}'.format(e, e.args[0]))
         return False
@@ -55,20 +37,20 @@ def createTable(address, username, password, database, table):
         return False
         
     return True
-    
+
+'''
+Executes CREATE TABLE command to generate new empty table
+'''
 def createTable(address, username, password, database, table):
 
     command = "CREATE TABLE " + table + "(NAME  VARCHAR(30) NOT NULL, NUMBER INT)"
     try:
-        # Open database connection
         conn = pymysql.connect(address, username, password, database)
-        # prepare a cursor object using cursor() method
         cursor = conn.cursor()
-        # Drop table if it already exist using execute() method.
         check_command = "DROP TABLE IF EXISTS " + table;
+        # execute check command to avoid error
         cursor.execute(check_command)
         cursor.execute(command)
-        # disconnect from server
         conn.close()
     except pymysql.InternalError as e:
         print('ERROR: {!r}, errno is {}'.format(e, e.args[0]))
@@ -76,21 +58,25 @@ def createTable(address, username, password, database, table):
     except pymysql.OperationalError as e:
         print('ERROR: {!r}, errno is {}'.format(e, e.args[0]))
         return False
+    except pymysql.ProgrammingError as e:
+        print('ERROR: {!r}, errno is {}'.format(e, e.args[0]))
+        return False
     except:
+        print('UNKNOWN ERROR')
         return False
         
     return True
     
+'''
+Executes SQL INSERT command to add one row to he table
+'''
 def insertIntoTable(address, username, password, database, table, name, number):
 
     command = "INSERT INTO " + table + "(NAME, NUMBER) VALUES('" + name + "', " + number + ")"
     try:
-        # Open database connection
         conn = pymysql.connect(address, username, password, database)
-        # prepare a cursor object using cursor() method
         cursor = conn.cursor()
         cursor.execute(command)
-        # disconnect from server
         conn.close()
     except pymysql.InternalError as e:
         print('ERROR: {!r}, errno is {}'.format(e, e.args[0]))
@@ -98,11 +84,50 @@ def insertIntoTable(address, username, password, database, table, name, number):
     except pymysql.OperationalError as e:
         print('ERROR: {!r}, errno is {}'.format(e, e.args[0]))
         return False
+    except pymysql.ProgrammingError as e:
+        print('ERROR: {!r}, errno is {}'.format(e, e.args[0]))
+        return False
     except:
+        print('UNKNOWN ERROR')
         return False
         
     return True
+
+'''
+Executes SQL SELECT command to read all data from the table
+'''
+def selectFromTable(address, username, password, database, table):
+
+    command = "SELECT * FROM " + table
+    try:
+        conn = pymysql.connect(address, username, password, database)
+        cursor = conn.cursor()
+        cursor.execute(command)
+        # fetch multiple row data
+        results = cursor.fetchall()
+        print(results)
+        conn.close()
+    except pymysql.InternalError as e:
+        print('ERROR: {!r}, errno is {}'.format(e, e.args[0]))
+        return False
+    except pymysql.OperationalError as e:
+        print('ERROR: {!r}, errno is {}'.format(e, e.args[0]))
+        return False
+    except pymysql.ProgrammingError as e:
+        print('ERROR: {!r}, errno is {}'.format(e, e.args[0]))
+        return False
+    except:
+        print('UNKNOWN ERROR')
+        return False
+        
+    return True    
     
+'''
+In main function connection with database is checked, then couple of operations are proceeded:
+ -> table TESTING is created (if table already exists then it is dropped)
+ -> three values are inserted into table TESTING
+ -> all values in table TESTING are fetched and displayed
+'''  
 def main():
     if(testConnection("localhost","root","","devops")):
         print("Connection tested with success")
@@ -110,6 +135,12 @@ def main():
         print("Table created with success")
     if(insertIntoTable("localhost","root","","devops", "TESTING", "Radoslaw", "1")):
         print("Row added successfully")
+    if(insertIntoTable("localhost","root","","devops", "TESTING", "Joe", "2")):
+        print("Row added successfully")
+    if(insertIntoTable("localhost","root","","devops", "TESTING", "Sue", "3")):
+        print("Row added successfully")
+    if(selectFromTable("localhost","root","","devops", "TESTING")):
+        print("Data fetched successfully")
     
        
 if __name__ == "__main__":
--- test/test_funs.py
+++ test/test_funs.py
@@ -4,32 +4,44 @@ Created on Fri Nov 15 10:57:14 2019
 
 @author: Radek
 """
+
+
 import sys
-sys.path.insert(1, 'D:/Dokumenty/Studia/Master2/DevOps/PytestProject')
+
+#################### MAKE CHANGES HERE #########################
+address = "localhost" # address of the database
+user = "root" # your username
+password = "" # your password
+database = "devops" # your database name
+table = "TESTING" # table name to store names and numbers (default "TESTING")
+sys.path.insert(1, 'D:/Dokumenty/Studia/Master2/DevOps/PytestProject') # path to testing file ("funs.py")
+########################### END ###############################
+
+#import testing module
 import funs
 
-address = "localhost"
-user = "root"
-password = ""
-database = "devops"
-table = "TESTING"
 
 def test_testConnection():  
-    assert funs.testConnection(address, user, password, "incorrect_db_name") == False
-    assert funs.testConnection(address, user, "incorrect_password", database) == False
-    assert funs.testConnection(address, "wrong_user", password, database) == False
-    assert funs.testConnection("made_up_address", user, password, database) == False
-    assert funs.testConnection(address, user, password, database) == True
+    assert funs.testConnection(address, user, password, "incorrect_db_name") == False # in case of wrong database name
+    assert funs.testConnection(address, user, "incorrect_password", database) == False # in case of incorrect password
+    assert funs.testConnection(address, "wrong_user", password, database) == False # in case of wrong login
+    assert funs.testConnection("made_up_address", user, password, database) == False # in case of wrong database address
+    assert funs.testConnection(address, user, password, database) == True # correct data
     
 def test_createTable():  
-    assert funs.createTable(address, user, password, database, table) == True
-    assert funs.createTable(address, user, password, database, "") == False
+    assert funs.createTable(address, user, password, database, table) == True # correct data
+    assert funs.createTable(address, user, password, database, "") == False # empty table name
     
 def test_insertIntoTable():
-    assert funs.insertIntoTable(address, user, password, database, table, "Radoslaw", "1") == True
-    assert funs.insertIntoTable(address, user, password, database, table, "Radoslaw", "integer") == False
-    assert funs.insertIntoTable(address, user, password, database, table, "Radoslaw", "") == False
+    assert funs.insertIntoTable(address, user, password, database, table, "Radoslaw", "1") == True # query executed with success
+    assert funs.insertIntoTable(address, user, password, database, table, "Joe", "2") == True # query executed with success
+    assert funs.insertIntoTable(address, user, password, database, table, "Sue", "3") == True # query executed with success
+    assert funs.insertIntoTable(address, user, password, database, table, "Radoslaw", "integer") == False # second parameter is not an integer
+    assert funs.insertIntoTable(address, user, password, database, table, "Radoslaw", "") == False # second parameter is not an integer
     
+def test_selectFromTable():
+    assert funs.selectFromTable(address, user, password, database, "") == False # no table specified
+    assert funs.selectFromTable(address, user, password, database, table) == True # correct data
     
 
 
