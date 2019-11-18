cac2ab8f8b59aaee30b4dfe113382cf46339aa95
radoslawik <radoslawpudelko96@gmail.com>
2019-11-18 15:29:05 +0100

    Added initial database connection

--- funs.py
+++ funs.py
@@ -5,5 +5,28 @@ Created on Fri Nov 15 10:55:58 2019
 @author: Radek
 """
 
+import pymysql
+
 def add(x,y):
-    return x+y;
+    return x+y
+    
+def main():
+    #open database connection
+    db = pymysql.connect("localhost","root","","company" )
+    print("Connected")
+      
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # execute SQL query using execute() method.
+    cursor.execute("SELECT VERSION()")
+
+    # Fetch a single row using fetchone() method.
+    data = cursor.fetchone()
+    print ("Database version : %s " % data)
+
+    # disconnect from server
+    db.close()
+    
+if __name__ == "__main__":
+    main()
